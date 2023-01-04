4cdd5d333fa8fcc984b4c011acfe0b62895966ed
IT1089778 <nvrnagella@gmail.com>
2023-01-04 23:26:13 +0530

    alligned commands

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@ cp ${var}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 
 
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 systemctl enable mongod
 systemctl restart mongod
