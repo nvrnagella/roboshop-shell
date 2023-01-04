b95c448bc895755573b47cba2fe6e3e0ce5c1513
IT1089778 <nvrnagella@gmail.com>
2023-01-04 13:43:56 +0530

    frontend is ready and nginx also ready

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,19 @@
-yum install nginx
+
+yum install nginx -y
+
 systemctl enable nginx
-systemctl start nginx
+
+sytemctl start nginx
+
+rm -rf /usr/share/nginx/html/*
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip
+
+cp /files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl restart nginx
+
