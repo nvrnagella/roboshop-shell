a49a90aecabc2164e401388df6dc37b09d88cdb5
IT1089778 <nvrnagella@gmail.com>
2023-01-05 09:01:18 +0530

    keeping own log display data

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,18 @@
 var=$(pwd)
+echo -e "\e[35m Install Nginx\e[0m"
 yum install nginx -y
-
+echo -e "\e[35m removing Nginx old content\e[0m"
 rm -rf /usr/share/nginx/html/*
-
+echo -e "\e[35m download frontend content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
 cd /usr/share/nginx/html
-
+echo -e "\e[35m extracting frontend content \e[0m"
 unzip /tmp/frontend.zip
-
+echo -e "\e[35m copy roboshop nginx config file \e[0m"
 cp ${var}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
-
+echo -e "\e[35m enable nginx \e[0m"
 systemctl enable nginx
-
+echo -e "\e[35m start nginx \e[0m"
 systemctl restart nginx
 
