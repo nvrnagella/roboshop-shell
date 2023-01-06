32e31602ea84008bb75c4cef0ad44f435831e364
IT1089778 <nvrnagella@gmail.com>
2023-01-06 11:04:06 +0530

    printing task is successful or not using $?

--- frontend.sh
+++ frontend.sh
@@ -2,18 +2,24 @@ script_location=$(pwd)
 LOG=/tmp/roboshop.log
 echo -e "\e[35m Install Nginx\e[0m"
 yum install nginx -y &>>${LOG}
+echo $?
 echo -e "\e[35m removing Nginx old content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
+echo $?
 echo -e "\e[35m download frontend content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-
+echo $?
 cd /usr/share/nginx/html
+echo $?
 echo -e "\e[35m extracting frontend content \e[0m"
 unzip /tmp/frontend.zip &>>${LOG}
+echo $?
 echo -e "\e[35m copy roboshop nginx config file \e[0m"
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
+echo $?
 echo -e "\e[35m enable nginx \e[0m"
 systemctl enable nginx &>>${LOG}
+echo $?
 echo -e "\e[35m start nginx \e[0m"
 systemctl restart nginx &>>${LOG}
-
+echo $?
