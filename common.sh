f0a822d5f5964f9819b57188b2fa58e87f650e40
IT1089778 <nvrnagella@gmail.com>
2023-01-06 17:44:50 +0530

    making bold

--- common.sh
+++ common.sh
@@ -3,9 +3,9 @@ LOG=/tmp/roboshop.log
 status_check() {
   if [ $? -eq 0 ]
   then
-    echo -e "\e[32m SUCCESS \e[0m"
+    echo -e "\e[1;32m SUCCESS \e[0m"
   else
-    echo -e "\e[31m FAILURE \e[0m"
+    echo -e "\e[1;31m FAILURE \e[0m"
     echo "refer log file for more information LOG - ${LOG}"
   exit
   fi
