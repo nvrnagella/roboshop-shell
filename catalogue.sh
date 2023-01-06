d1530866a86f71ef8101a745a48db96a09396367
IT1089778 <nvrnagella@gmail.com>
2023-01-06 17:37:28 +0530

    imported common.sh file

--- catalogue.sh
+++ catalogue.sh
@@ -1,15 +1,5 @@
-script_location=$(pwd)
-LOG=/tmp/roboshop.log
-status_check() {
-  if [ $? -eq 0 ]
-  then
-    echo -e "\e[32m SUCCESS \e[0m"
-  else
-    echo -e "\e[31m FAILURE \e[0m"
-    echo "refer log file for more information LOG - ${LOG}"
-  exit
-  fi
-}
+source common.sh
+
 echo -e "\e[31m configuring nodejs repo \e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
 status_check
--- frontend.sh
+++ frontend.sh
@@ -1,59 +1,23 @@
-script_location=$(pwd)
-LOG=/tmp/roboshop.log
+source common.sh
 echo -e "\e[35m Install Nginx\e[0m"
 yum install nginx -y &>>${LOG}
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check
 echo -e "\e[35m removing Nginx old content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check
 echo -e "\e[35m download frontend content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check
 cd /usr/share/nginx/html
 echo -e "\e[35m extracting frontend content \e[0m"
 unzip /tmp/frontend.zip &>>${LOG}
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check
 echo -e "\e[35m copy roboshop nginx config file \e[0m"
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check
 echo -e "\e[35m enable nginx \e[0m"
 systemctl enable nginx &>>${LOG}
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check
 echo -e "\e[35m start nginx \e[0m"
 systemctl restart nginx &>>${LOG}
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check
