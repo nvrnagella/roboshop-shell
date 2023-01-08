40cba63d25fb80185307066838e4d21ea2032dd1
IT1089778 <nvrnagella@gmail.com>
2023-01-08 20:07:09 +0530

    update nginx roboshop conf file

--- catalogue.sh
+++ catalogue.sh
@@ -3,49 +3,63 @@ source common.sh
 print_head "configuring nodejs application"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
 status_check
+
 print_head "installing nodejs"
 yum install nodejs -y &>>${LOG}
 status_check
+
 print_head "add application user"
 id roboshop &>>${LOG}
 if [ $? -ne 0 ]; then
   useradd roboshop &>>${LOG}
 fi
 status_check
+
 mkdir -p /app &>>${LOG}
 status_check
+
 print_head "downloading app contents"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${LOG}
 status_check
+
 print_head "clean old app contents"
 rm -rf /app/* &>>${LOG}
 status_check
+
 cd /app
 print_head "extracting app data"
 unzip /tmp/catalogue.zip &>>${LOG}
 status_check
+
 cd /app
 print_head "installing npm"
 npm install &>>${LOG}
 status_check
+
 print_head "configuring catalogue service"
 cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>${LOG}
 status_check
+
 print_head "reloading daemon"
 systemctl daemon-reload &>>${LOG}
 status_check
+
 print_head "enabling catalogue"
 systemctl enable catalogue &>>${LOG}
 status_check
+
 print_head "starting catalogue"
 systemctl start catalogue &>>${LOG}
 status_check
+
 print_head "copying mongo repo"
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
 status_check
+
 print_head "installing mongo client"
 yum install mongodb-org-shell -y &>>${LOG}
 status_check
+
 print_head "loading database schema"
 mongo --host mongodb-dev.nvrnagella.online </app/schema/catalogue.js &>>${LOG}
 status_check
