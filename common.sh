024ba19fd49c72eaaf916c3afd2b10373b31fc55
IT1089778 <nvrnagella@gmail.com>
2023-01-09 01:41:27 +0530

    mysql server code

--- common.sh
+++ common.sh
@@ -15,14 +15,7 @@ print_head() {
   echo -e "\e[1m $1 \e[0m"
 }
 
-NODEJS (){
-  print_head "configuring nodejs application"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
-  status_check
-
-  print_head "installing nodejs"
-  yum install nodejs -y &>>${LOG}
-  status_check
+APP_PREREQ (){
 
   print_head "add application user"
   id roboshop &>>${LOG}
@@ -32,7 +25,6 @@ NODEJS (){
   status_check
 
   mkdir -p /app &>>${LOG}
-  status_check
 
   print_head "downloading app contents"
   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG}
@@ -46,11 +38,9 @@ NODEJS (){
   print_head "extracting app data"
   unzip /tmp/${component}.zip &>>${LOG}
   status_check
+}
 
-  cd /app
-  print_head "installing npm"
-  npm install &>>${LOG}
-  status_check
+SYSTEMD_SETUP (){
 
   print_head "configuring ${component} service"
   cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
@@ -67,17 +57,76 @@ NODEJS (){
   print_head "starting ${component}"
   systemctl start ${component} &>>${LOG}
   status_check
+}
+
+LOAD_SCHEMA (){
+
   if [ ${schema_load} == "true" ]; then
-    print_head "copying mongo repo"
-    cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
-    status_check
+    if [ ${schema_type} == "mongo" ]; then
+      print_head "copying mongo repo"
+      cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
+      status_check
 
-    print_head "installing mongo client"
-    yum install mongodb-org-shell -y &>>${LOG}
-    status_check
+      print_head "installing mongo client"
+      yum install mongodb-org-shell -y &>>${LOG}
+      status_check
 
-    print_head "loading database schema"
-    mongo --host mongodb-dev.nvrnagella.online </app/schema/${component}.js &>>${LOG}
-    status_check
+      print_head "loading database schema"
+      mongo --host mongodb-dev.nvrnagella.online </app/schema/${component}.js &>>${LOG}
+      status_check
+    fi
+    if [ ${schema_type} == "mysql" ]; then
+
+      print_head "installing mysql client"
+      yum install mysql -y &>>${LOG}
+      status_check
+
+      print_head "loading database schema"
+      mysql -h mysql-dev.nvrnagella.online -uroot -p${root_mysql_password} < /app/schema/shipping.sql  &>>${LOG}
+      status_check
+    fi
   fi
+
+}
+
+NODEJS (){
+  print_head "configuring nodejs application"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+  status_check
+
+  print_head "installing nodejs"
+  yum install nodejs -y &>>${LOG}
+  status_check
+
+  APP_PREREQ
+
+  cd /app
+  print_head "installing nodejs dependencies"
+  npm install &>>${LOG}
+  status_check
+
+  SYSTEMD_SETUP
+
+
+}
+
+MAVEN (){
+
+  print_head "installing MAVEN"
+  yum install maven -y &>>${LOG}
+  status_check
+
+  APP_PREREQ
+
+  print_head "build a package"
+  mvn clean package &>>${LOG}
+  status_check
+
+  print_head "copy app file to app location"
+  mv target/${component}-1.0.jar ${component}.jar
+  status_check
+
+  SYSTEMD_SETUP
+
+  LOAD_SCHEMA
 }
