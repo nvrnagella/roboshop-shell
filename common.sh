8a8bfa5452af31639e8fd0e5402cd0fcbc1d6a72
IT1089778 <nvrnagella@gmail.com>
2023-01-08 21:29:53 +0530

    code reuse common file for user and cataloue

--- common.sh
+++ common.sh
@@ -14,3 +14,70 @@ status_check() {
 print_head() {
   echo -e "\e[1m $1 \e[0m"
 }
+
+NODEJS (){
+  print_head "configuring nodejs application"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+  status_check
+
+  print_head "installing nodejs"
+  yum install nodejs -y &>>${LOG}
+  status_check
+
+  print_head "add application user"
+  id roboshop &>>${LOG}
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>${LOG}
+  fi
+  status_check
+
+  mkdir -p /app &>>${LOG}
+  status_check
+
+  print_head "downloading app contents"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG}
+  status_check
+
+  print_head "clean old app contents"
+  rm -rf /app/* &>>${LOG}
+  status_check
+
+  cd /app
+  print_head "extracting app data"
+  unzip /tmp/${component}.zip &>>${LOG}
+  status_check
+
+  cd /app
+  print_head "installing npm"
+  npm install &>>${LOG}
+  status_check
+
+  print_head "configuring ${component} service"
+  cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
+  status_check
+
+  print_head "reloading daemon"
+  systemctl daemon-reload &>>${LOG}
+  status_check
+
+  print_head "enabling ${component}"
+  systemctl enable ${component} &>>${LOG}
+  status_check
+
+  print_head "starting ${component}"
+  systemctl start ${component} &>>${LOG}
+  status_check
+
+  print_head "copying mongo repo"
+  cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
+  status_check
+
+  print_head "installing mongo client"
+  yum install mongodb-org-shell -y &>>${LOG}
+  status_check
+
+  print_head "loading database schema"
+  mongo --host mongodb-dev.nvrnagella.online </app/schema/${component}.js &>>${LOG}
+  status_check
+
+}
